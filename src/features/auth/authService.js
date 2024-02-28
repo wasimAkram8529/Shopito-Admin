6ee509d9869f60afcc48d91b41ad7987bc6062a7
wasimAkram8529 <codelikearocstar@gmail.com>
2024-02-28 12:31:44 +0530

    URL Change

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 // import { base_url } from "../../utils/base_url";
 
-const BACKEND_URL =
-  `https://shopito - admin.netlify.app` || process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const API_URL = `${BACKEND_URL}/api/user/`;
 
 const login = async (userData) => {
